cb462d5864e4c5dfe9f4fff64d41e1bb84a7cdfd
yang <liyang0727@hotmail.com>
2017-12-24 17:40:55 -0500

    added tree and BFS solution

--- days/day24.py
+++ days/day24.py
@@ -2,6 +2,7 @@ import os
 import re
 import copy
 import operator
+from collections import deque
 
 INPUT_DIRECTORY = '../inputs/'
 INPUT_FILE_EXTENSION = '_input.txt'
@@ -78,8 +79,100 @@ def part_two(currenpipest_file):
     key = max(map.iteritems(), key=operator.itemgetter(0))[0]
     return map[key]
 
+# Tree solution
+class Pipe(object):
+    def __init__(self, pipe, next_port):
+        self.pipe = pipe
+        self.next_port = next_port
+        self.pipes = []
+
+def backtrace(parent, start, end):
+    path = [end]
+    while path[-1] != start:
+        path.append(parent[path[-1]])
+    return path
+
+def construct(pipes):
+    parent = {}
+    root = Pipe((0, 0), 0)
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        node = queue.popleft()
+        next_port = node.next_port
+        used_pipes = backtrace(parent, root, node)
+        remaining_pipes = [(x, y) for x, y in pipes if x == next_port or y == next_port]
+        for used in used_pipes:
+            if used.pipe in remaining_pipes:
+                remaining_pipes.remove(used.pipe)
+        for remaining_pipe in remaining_pipes:
+            new_port = 0
+            if remaining_pipe[0] == next_port:
+                new_port = remaining_pipe[1]
+            else:
+                new_port = remaining_pipe[0]
+            next_node = Pipe(remaining_pipe, new_port)
+            parent[next_node] = node
+            node.pipes.append(next_node)
+            queue.append(next_node)
+    return root
+
+def sum_ports(nodes):
+    count = 0
+    for node in nodes:
+        count += node.pipe[0] + node.pipe[1]
+    return count
+
+def print_nodes(nodes):
+    pipes = []
+    for node in nodes:
+        pipes.append(node.pipe)
+    print pipes
+
+def find_strongest(root):
+    parent = {}
+    strongest = []
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        node = queue.popleft()
+        if not node.pipes:
+            strongest.append(sum_ports(backtrace(parent, root, node)))
+        for n in node.pipes:
+            parent[n] = node
+            queue.append(n)
+    return max(strongest)
+
+def find_longest(root):
+    parent = {}
+    longest = {}
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        node = queue.popleft()
+        if not node.pipes:
+            nodes = backtrace(parent, root, node)
+            if len(nodes) not in longest:
+                longest[len(nodes)] = sum_ports(nodes)
+            else:
+                longest[len(nodes)] = max(longest[len(nodes)], sum_ports(nodes))
+        for n in node.pipes:
+            parent[n] = node
+            queue.append(n)
+    key = max(longest.iteritems(), key=operator.itemgetter(0))[0]
+    return longest[key]
+
+def print_nodes(nodes):
+    pipes = []
+    for node in nodes:
+        pipes.append(node.pipe)
+    print pipes
+
 if __name__ == '__main__':
     current_file = os.path.splitext(os.path.basename(__file__))[0]
     pipes = load_input(current_file + INPUT_FILE_EXTENSION)
-    print part_one(pipes)
-    print part_two(pipes)
+    # print part_one(pipes)
+    # print part_two(pipes)
+    root = construct(pipes)
+    print find_strongest(root)
+    print find_longest(root)
